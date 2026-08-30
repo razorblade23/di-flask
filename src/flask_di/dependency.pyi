b7caecb623125567b2688bcb65e02e75cdc78d8d
razorblade23 <81590614+razorblade23@users.noreply.github.com>
2026-08-30 21:00:57 +0200

    Delete src/flask_di/dependency.pyi

--- src/flask_di/dependency.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Generic, Type, TypeVar
-
-T = TypeVar("T")
-
-class Provide(Generic[T]):
-    dependency_type: Type[T]
-
-    def __init__(self, dependency_type: Type[T]) -> None: ...
-    def resolve(self) -> T: ...
